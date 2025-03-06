ab531dd87440b671ca1340cd543c311c55455ead
Michael <dasilvatobi12@gmail.com>
2025-03-06 04:51:19 +0100

    Update About.jsx

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 
+
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+      <h1 className="text-[60px] text-[bold]">About us</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit placeat id, quaerat nobis qui harum libero fugit ullam dolorem amet, dicta delectus dolores provident nulla autem architecto aperiam recusandae nam quas, dolorum ab. Neque, reprehenderit nobis at praesentium hic corrupti explicabo distinctio animi. Provident iure sint magnam officiis harum voluptatibus!</p>
+      <div className="flex ">
+        <p>GET INVOLVED</p>
+        <p className='pl-[70px]'>OUR PASTORS</p>
+      </div>
+    </div>
   )
 }
 
