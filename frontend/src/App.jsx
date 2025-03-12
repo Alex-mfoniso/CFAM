1b1ee770dd3c6d5fd663adb5d6f460d389c43fd1
Daniel Anyamene <dancolt22@gmail.com>
2025-03-12 11:04:53 -0700

    Added contacts page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import AppRoutes from "./routes"; // Ensure this file exists
 import { Analytics } from "@vercel/analytics/react";
+import Contact from "./pages/Contact";
+
 
 const App = () => {
   return (
